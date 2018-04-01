557cf861b846d0f40a75d56b4628fe8d047377e3
Jiří Furda <Furdys@users.noreply.github.com>
2018-04-01 23:45:21 +0200

    Release candidate

--- proj2.sql
+++ proj2.sql
@@ -1,10 +1,3 @@
-/* TO-DO
- * Default value for DATE
- * Unsigned number
- * hrazeni_castka NULL or NOT NULL?
- * duplicity in hrazeni, dodavani, uskladneni
-*/
-
 ----- Entity -----
 
 CREATE TABLE pojistovny
@@ -52,11 +45,12 @@ CREATE TABLE rezervace
 
 CREATE TABLE prodeje
 (
-	prodej_id		NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	prodej_datum	DATE NOT NULL,
-	prodej_mnozstvi	NUMBER(5) NOT NULL,
-	lek_id			NUMBER NOT NULL,
-	pobocka_id		NUMBER NOT NULL,
+	prodej_id			NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	prodej_datum		DATE NOT NULL,
+	prodej_mnozstvi		NUMBER(5) NOT NULL,
+	prodej_rodne_cislo	NUMBER(10),
+	lek_id				NUMBER NOT NULL,
+	pobocka_id			NUMBER NOT NULL,
 	CONSTRAINT prodeje_fk_lek
 		FOREIGN KEY (lek_id)
 		REFERENCES leky(lek_id),
@@ -74,7 +68,7 @@ CREATE TABLE hrazeni
 (
 	pojistovna_cislo	NUMBER(3) NOT NULL,
 	lek_id				NUMBER NOT NULL,
-	hrazeni_castka		NUMBER(5) NOT NULL,
+	hrazeni_castka		NUMBER(5),
 	CONSTRAINT hrazeni_fk_pojistovna
 		FOREIGN KEY (pojistovna_cislo)
 		REFERENCES pojistovny(pojistovna_cislo),
@@ -86,7 +80,7 @@ CREATE TABLE hrazeni
 CREATE TABLE dodavani
 (
 	dodavatel_id	NUMBER NOT NULL,
-	lek_id				NUMBER NOT NULL,
+	lek_id			NUMBER NOT NULL,
 	dodavani_cena	NUMBER(5) NOT NULL,
 	CONSTRAINT dodavani_fk_dodavatel
 		FOREIGN KEY (dodavatel_id)
@@ -108,6 +102,63 @@ CREATE TABLE uskladneni
 		FOREIGN KEY (pobocka_id)
 		REFERENCES pobocky(pobocka_id)
 );
+
+
+
+----- Data ----
+INSERT INTO pojistovny(pojistovna_cislo, pojistovna_jmeno)
+VALUES (554, 'Union');
+
+INSERT INTO pojistovny(pojistovna_cislo, pojistovna_jmeno)
+VALUES (499, 'CSOB');
+
+INSERT INTO pojistovny(pojistovna_cislo, pojistovna_jmeno)
+VALUES (888, 'VZP');
+
+INSERT INTO dodavatele(dodavatel_nazev)
+VALUES ('Bayer');
+
+INSERT INTO dodavatele(dodavatel_nazev)
+VALUES ('GlaxoSmithKline');
+
+INSERT INTO dodavatele(dodavatel_nazev)
+VALUES ('Lundbeck');
+
+INSERT INTO leky(lek_nazev, lek_nutnost_predpisu, lek_cena)
+VALUES ('PARADIM', 0, 49);
+
+INSERT INTO leky(lek_nazev, lek_nutnost_predpisu, lek_cena)
+VALUES ('PABAL', 0, 88);
+
+INSERT INTO leky(lek_nazev, lek_nutnost_predpisu, lek_cena)
+VALUES ('IALUGEN PLUES', 0, 22);
+
+INSERT INTO pobocky(pobocka_jmeno, pobocka_adresa)
+VALUES ('The pharmacy market', 'Gorkeho 22, 602 00 Brno-stred, Cesko');
+
+INSERT INTO pobocky(pobocka_jmeno, pobocka_adresa)
+VALUES ('Pharmacy Aesculap', 'Dornych 404/4, 602 00 Brno, Èesko');
+
+INSERT INTO rezervace(rezervace_jmeno_zakaznika, rezervace_datum, rezervace_mnozstvi, lek_id, pobocka_id)
+VALUES
+('Peter Havan', date '2018-04-01', 1, (SELECT lek_id from leky WHERE lek_nazev='PABAL'), (SELECT pobocka_id from pobocky Where pobocka_jmeno='Pharmacy Aesculap'));
+
+INSERT INTO prodeje(prodej_datum, prodej_mnozstvi, lek_id, pobocka_id)
+VALUES(date '2018-04-02', 1, (SELECT lek_id from leky WHERE lek_nazev='PABAL'),  (SELECT pobocka_id from pobocky Where pobocka_jmeno='Pharmacy Aesculap'));
+
+INSERT INTO hrazeni(pojistovna_cislo, lek_id, hrazeni_castka)
+VALUES((SELECT pojistovna_cislo from pojistovny WHERE pojistovna_jmeno='Union'), (SELECT lek_id from leky WHERE lek_nazev='PABAL'), 22);
+
+INSERT INTO dodavani(dodavatel_id, lek_id, dodavani_cena)
+VALUES((SELECT dodavatel_id from dodavatele WHERE dodavatel_nazev='Bayer'), (SELECT lek_id from leky WHERE lek_nazev='PABAL'), 5);
+
+INSERT INTO uskladneni(lek_id, pobocka_id, uskladneni_mnozstvi)
+VALUES((SELECT lek_id from leky WHERE lek_nazev='PABAL'), (SELECT pobocka_id from pobocky Where pobocka_jmeno='Pharmacy Aesculap'), 15);
+
+INSERT INTO uskladneni(lek_id, pobocka_id, uskladneni_mnozstvi)
+VALUES((SELECT lek_id from leky WHERE lek_nazev='PARADIM'), (SELECT pobocka_id from pobocky Where pobocka_jmeno='Pharmacy Aesculap'), 15);
+
+
 
 /*
 ===== For resetting database =====
